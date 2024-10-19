45961ac3cfe235e347b9f236fa7fedb1013d54f0
Kirill <kirill59rus368@yandex.ru>
2024-10-19 19:22:16 +0300

    Create button for open modal

---
+++ src/stories/ModalButton/ModalButton.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta } from "@storybook/react";
+import ModalButton from "./ModalButton";
+
+const meta: Meta<typeof ModalButton> = {
+    title: "Homework4/General/ModalButton",
+    component: ModalButton,
+    // argTypes: {
+    //     children: {
+    //         type: "string",
+    //         defaultValue: "some value"
+    //     } 
+    // }
+}
+
+export default meta
+
+export const primary = {
+
+}
