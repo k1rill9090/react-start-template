ba47f05554e94e9ceb8712e927e68a61816bce94
Kirill <kirill59rus368@yandex.ru>
2024-10-24 00:49:13 +0300

    Add Provider for dark theme in App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
+import { ThemeProvider } from './homeworks/ThemeSwitcher/ThemeContext/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
