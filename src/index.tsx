830bdcf33d43794b162d81045ad9eaa06d3b96ca
k1rill9090 <102020328+k1rill9090@users.noreply.github.com>
2024-10-27 21:05:39 +0300

    Revert "Add Provider for dark theme in App component"
    This reverts commit 6a387a115e3d0d93f1e3d1d8284c99d505141213.

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
-import { ThemeProvider } from './homeworks/ThemeSwitcher/ThemeContext/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
