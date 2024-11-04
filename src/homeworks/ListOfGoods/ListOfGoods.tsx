512925e221ca64cd7df49417a5a65965c1c24057
Kirill <kirill59rus368@yandex.ru>
2024-11-04 19:57:32 +0300

    fix path to ModalContent for storybook and check code with es-lint

--- src/homeworks/ListOfGoods/ListOfGoods.tsx
+++ src/homeworks/ListOfGoods/ListOfGoods.tsx
@@ -1,60 +1,51 @@
-import React, { FC, useEffect, useRef, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { createRandomProduct, Product } from '../ts1/3_write';
 import ShortCardGood from '../jsxCss/onlineStore/ShortCardGood/ShortCardGood';
-import styles from './ListOfGoods.module.sass'
+import styles from './ListOfGoods.module.sass';
 import { useTheme } from '../ThemeSwitcher/ThemeContext/ThemeContext';
 import clsx from 'clsx';
 
-
 interface ListOfGoodsProps {
-    goods: Array<Product>
+  goods: Array<Product>;
 }
 
-const ListOfGoods: FC<ListOfGoodsProps>= ({goods}) => {
-    const {theme} = useTheme();
+const ListOfGoods: FC<ListOfGoodsProps> = ({ goods }) => {
+  const { theme } = useTheme();
 
-    const [goodsArr, setGoodsArr] = useState(goods);
-    const addGood = () => {
-        const newGood = createRandomProduct("28.10.2024");
-        setGoodsArr([...goodsArr, newGood]);
+  const [goodsArr, setGoodsArr] = useState(goods);
+  const addGood = () => {
+    const newGood = createRandomProduct('28.10.2024');
+    setGoodsArr([...goodsArr, newGood]);
+  };
+
+  const lastElem = useRef(null);
+  const observer = useRef(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+    const callback = (entries: Array<IntersectionObserverEntry>) => {
+      if (entries[0].isIntersecting) {
+        addGood();
+      }
     };
-
-    const lastElem = useRef(null);
-    const observer = useRef(null);
-
-    useEffect(() => {
-        if (observer.current) observer.current.disconnect();
-        let callback = (entries: Array<IntersectionObserverEntry>) => {
-            if (entries[0].isIntersecting) {
-                addGood()            
-            }
-        };
-        observer.current = new IntersectionObserver(callback)
-        observer.current.observe(lastElem.current)
-    }, [goodsArr])
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElem.current);
+  }, [goodsArr]);
 
   return (
     <div className={styles[`background-${theme}`]}>
-        <h2>Список товаров</h2>
-    <div>
-        {
-            goodsArr.map(
-                (p) => <ShortCardGood
-                    key={p.id} 
-                    sum={p.price} 
-                    img={p.photo} 
-                    name={p.name} 
-                    desc={p.desc}
-                    theme={theme}
-                />
-            )
-        }
-        
+      <h2>Список товаров</h2>
+      <div>
+        {goodsArr.map((p) => (
+          <ShortCardGood key={p.id} sum={p.price} img={p.photo} name={p.name} desc={p.desc} theme={theme} />
+        ))}
+      </div>
+      <button className={clsx([styles.btn, styles[`btn-${theme}`]])} onClick={() => addGood()}>
+        Показать еще
+      </button>
+      <div ref={lastElem} className={styles.observerDiv}></div>
     </div>
-    <button className={clsx([styles.btn, styles[`btn-${theme}`]])} onClick={() => addGood()}>Показать еще</button>
-    <div ref={lastElem} className={styles.observerDiv}></div>
-    </div>
-  )
-}
+  );
+};
 
-export default ListOfGoods
+export default ListOfGoods;
