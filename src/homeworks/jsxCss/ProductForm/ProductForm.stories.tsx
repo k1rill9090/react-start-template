703d8d7690c64dfddaa564392d18a353e74791b1
Kirill <kirill59rus368@yandex.ru>
2024-11-26 00:35:05 +0300

    Add create product form

---
+++ src/homeworks/jsxCss/ProductForm/ProductForm.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta } from '@storybook/react';
+import ContextLayout from '../../../stories/ContextLayout/ContextLayout';
+import React from 'react';
+import styles from './ProductForm.module.sass'
+import ProductForm from './ProductForm';
+
+const meta: Meta<typeof ContextLayout> = {
+  title: 'Homework10_forms/CreateProductForm',
+  component: ContextLayout,
+};
+export default meta;
+
+export const primary = {
+  args: {
+    children: (
+        <div className={styles.story}>
+            <ProductForm/>
+        </div>
+      
+    ),
+  },
+};
