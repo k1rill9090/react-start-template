8aeea4202212b4950c44b5812da05cbb25ec66d6
Kirill <kirill59rus368@yandex.ru>
2024-11-27 00:35:29 +0300

    Add my auth form

---
+++ src/homeworks/jsxCss/MyAuthForm/MyAuthForm.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta } from '@storybook/react';
+import ContextLayout from '../../../stories/ContextLayout/ContextLayout';
+import React from 'react';
+import styles from './MyAuthForm.module.sass'
+import MyAuthForm from './MyAuthForm';
+
+const meta: Meta<typeof ContextLayout> = {
+  title: 'Homework10_forms/MyAuthForm',
+  component: ContextLayout,
+};
+export default meta;
+
+export const primary = {
+  args: {
+    children: (
+        <div className={styles.story}>
+            <MyAuthForm/>
+        </div>
+      
+    ),
+  },
+};
